35a45ad76cc84f61bd007ddfffbb67826448a698
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-18 20:50:42 +0300

    doing final work on hello

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -3,5 +3,7 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        // I am making some more changes and i will make commit and push
+        System.out.println("EU08 lEARNING GIT&GITHUB");
     }
 }
