eb5089786ad23bbd4d14bf2236d5ed8c8ce7be6a
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-19 10:46:47 +0300

    TestCase004  step4

--- src/hello/TestCase004.java
+++ src/hello/TestCase004.java
@@ -2,6 +2,7 @@ package hello;
 
 public class TestCase004 {
     // I will try again to merge
-    // i didnt get a conflict wiht tc003
+    // i didnt get a conflict with tc003
+    //what is going on
 
 }
