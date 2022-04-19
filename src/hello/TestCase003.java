765b3ed2005218cc5af07dbc591e3854f8b28c6c
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-19 10:24:37 +0300

    TestCase003 of master step 2

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -1,6 +1,6 @@
 package hello;
 
 public class TestCase003 {
-    // i created a class named
-    //
+    // i created a class named testcase003
+    // but also tc003 brancj has  a class witjh the same name
 }
