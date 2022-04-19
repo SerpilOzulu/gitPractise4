9c038172054fe5d2c7265f3b27df50a130dae91c
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-19 14:55:15 +0300

    TestCase005  third commit

--- src/hello/TestCase005.java
+++ src/hello/TestCase005.java
@@ -3,4 +3,5 @@ package hello;
 public class TestCase005 {
     // this is the first commit of testcase005
     // we are trying to get the git flows for team collaboration
+    // we will also make a pull request
 }
