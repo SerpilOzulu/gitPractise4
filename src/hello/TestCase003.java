4fe27a9a0a60873a89ed80b71272979cc0a90fdb
SerpilOzulu <serpilozturk000@gmail.com>
2022-04-19 10:25:09 +0300

    TestCase003 of master step 3

--- src/hello/TestCase003.java
+++ src/hello/TestCase003.java
@@ -3,4 +3,5 @@ package hello;
 public class TestCase003 {
     // i created a class named testcase003
     // but also tc003 brancj has  a class witjh the same name
+    // i did it bcs i want to see a merge conflict
 }
